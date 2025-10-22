61e37cda177f61390ce5df1e48c0fd9fc3e0e0a9
Daffaazzi <azzidaffa@gmail.com>
2025-10-22 12:48:02 +0700

    percobaan 2

--- SiakadFor08.java
+++ SiakadFor08.java
@@ -2,24 +2,23 @@ import java.util.Scanner;
 
 public class SiakadFor08 {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        
-        double nilai,tertinggi=0, terendah=100;
+        Scanner sc = new Scanner(System.in);
 
+        double nilai,tertinggi = 0, terendah = 100;
         for (int i = 1; i <= 10; i++) {
-            System.out.print("Masukkan nilai mahasiswa ke-" + i + ": ");
-            nilai = scanner.nextDouble();
-            
-            if (nilai > tertinggi) {
+            System.out.print("Masukkan nilai mahasiswa ke-" + i + ":" );
+            nilai = sc.nextDouble();
+            if (nilai>tertinggi);{
                 tertinggi = nilai;
             }
-            if (nilai < terendah) {
+            if (nilai<terendah);{
                 terendah = nilai;
-
-        System.out.println("Nilai tertinggi: " + tertinggi);
-        System.out.println("Nilai terendah: " + terendah);
             }
         }
-    }
 
-}
+            System.out.println("Nilai tertinggi: " + tertinggi);
+            System.out.println("Nilai terendah: "  + terendah);
+        }
+     }
+        
+    
